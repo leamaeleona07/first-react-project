d7b43dc0a77f0dc917408883b756e13de33a8b33
lea mae <leona.leamae7@gmail.com>
2025-09-10 17:51:20 +0800

    first commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+const Navbar = () => {
+    return (
+        <nav>
+            <div className=""></div>
+            <ul>
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>CONTACT</li>
+            </ul>
+        </nav>
+    )
+}
+
+export default Navbar
