3f97146341f23bb1e827edb71dc4f89610eb35f4
unknown <Lea Mae>
2025-09-22 12:58:58 +0800

    :sparkles: updated ip, added style

--- src/components/BlogDetails.jsx
+++ src/components/BlogDetails.jsx
@@ -12,14 +12,16 @@ const BlogDetails = () => {
     )
   };
   return (
-    <div className="Blog-details">
+    <div className="Blog-Details">
       
       {error && <div>{error}</div>}
       {blogs && (
-        <article>
-          <h2>{blogs.title}</h2>
-                  <p>written by {blogs.author}</p>
-                  <div> {blogs.body}</div>
+        <article className="flex flex-col items-center h-screen">
+          <h2 className="text-3xl font-bold mb-4 text-pink-400">
+            {blogs.title}
+            </h2>
+                  <p className="text-gray-500">written by {blogs.author}</p>
+                  <div className="mt-10 text-lg leading-relaxed indent-8"> {blogs.body}</div>
         </article>
       )}
     </div>
