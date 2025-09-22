61152c819a2358560cf7e1d00c6c95349132e101
unknown <Lea Mae>
2025-09-22 12:20:51 +0800

    changed ip add

--- vite.config.js
+++ vite.config.js
@@ -5,6 +5,6 @@ import tailwindcss from '@tailwindcss/vite'
 export default defineConfig({
   plugins: [react(), tailwindcss(),],
   server: {
-    host: '192.168.122.157',
+    host: '0.0.0.0',
   }
 })
